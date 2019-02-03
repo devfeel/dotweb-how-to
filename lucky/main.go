4f7bc3848200f68a26f9bbc18bfaa8280df0d25b
pzrr@qq.com <pzrr@qq.com>
2019-02-03 09:15:05 +0800

    add how to use ConfigSet tools in your app

--- lucky/main.go
+++ lucky/main.go
@@ -25,10 +25,11 @@ func init() {
 	fmt.Println(` / /_/ / / /_/ // /_  | |/ |/ / /  __/ / /_/ /`)
 	fmt.Println(`/_____/  \____/ \__/  |__/|__/  \___/ /_.___/`)
 	fmt.Println(``)
-	fmt.Println(`              → 2019新春抽奖活动 ←              `)
-	fmt.Println(`https://github.com/devfeel/dotweb/issues/181`)
+	fmt.Println(`             → 2019新春抽奖活动 ←              `)
+	fmt.Println(`     https://github.com/devfeel/dotweb      `)
 	fmt.Println(``)
 
+	fmt.Println(`活动地址: https://github.com/devfeel/dotweb/issues/181`)
 	fmt.Println("初始化名单，共计", len(userPool), "人")
 	fmt.Println("名单公示：", userPool)
 	fmt.Println(``)
