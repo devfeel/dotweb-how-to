d3036b85e1c527ca24a5ac6e0501515d87fb7855
pzrr@qq.com <pzrr@qq.com>
2019-02-11 15:31:12 +0800

    update lucky

--- lucky/main.go
+++ lucky/main.go
@@ -10,7 +10,9 @@ import (
 var userPool map[string]int
 var luckySeed map[int]string
 var totalSeed int
-var userNames string = "bibibi-bug,dotqi,WithLin,youzizzz,zcmiao190,wbly7758,misasky,qizhenshuai,motanzg,Wooshawn,itstudying,vhuyuy,jokerddj,tianguozhifeng,SHIJIEZ,gumochengxing,dfy167,LyricTian,hades2013,vinnking,wweir,violetgo,Imhven,xskingdeekis,wangyun111,memoryxyd,chplain,willxuecn,yibalaodao,xiang2lin,lyw1995,hacfox,xingliuhua,yrx1354,mfcab,hunterfall,solarhell,thesilent,lhkzx007,171869092,haphupan,921034403,yuzhoutong,baidw,Sakurasan,NightFarmer,imxyb,xiam123,qbmiller,Uncho,LinlinDu,shaocongcong,sayhei,lvchengchang,le0l1,1509477745,dcwang1989,swing4,archer-bao,sszgr,83103005,qq976739120,aredcup,sowhaton,angusguo,royromny,wx7217242,thhy,rongxr,MonkeyInWind,Fang-Li,hbk100"
+
+//var userNames string = "bibibi-bug,dotqi,WithLin,youzizzz,zcmiao190,wbly7758,misasky,qizhenshuai,motanzg,Wooshawn,itstudying,vhuyuy,jokerddj,tianguozhifeng,SHIJIEZ,gumochengxing,dfy167,LyricTian,hades2013,vinnking,wweir,violetgo,Imhven,xskingdeekis,wangyun111,memoryxyd,chplain,willxuecn,yibalaodao,xiang2lin,lyw1995,hacfox,xingliuhua,yrx1354,mfcab,hunterfall,solarhell,thesilent,lhkzx007,171869092,haphupan,921034403,yuzhoutong,baidw,Sakurasan,NightFarmer,imxyb,xiam123,qbmiller,Uncho,LinlinDu,shaocongcong,sayhei,lvchengchang,le0l1,1509477745,dcwang1989,swing4,archer-bao,sszgr,83103005,qq976739120,aredcup,sowhaton,angusguo,royromny,wx7217242,thhy,rongxr,MonkeyInWind,Fang-Li,hbk100"
+var userNames string = "bibibi-bug,dotqi,WithLin,youzizzz,zcmiao190,wbly7758,misasky,qizhenshuai,motanzg,Wooshawn,itstudying,vhuyuy,jokerddj,tianguozhifeng,SHIJIEZ,gumochengxing,dfy167,LyricTian,hades2013,vinnking,wweir,violetgo,Imhven,xskingdeekis,wangyun111,memoryxyd,chplain,willxuecn,yibalaodao,xiang2lin,hacfox,xingliuhua,yrx1354,mfcab,hunterfall,solarhell,thesilent,lhkzx007,171869092,haphupan,921034403,yuzhoutong,baidw,Sakurasan,NightFarmer,imxyb,xiam123,qbmiller,Uncho,LinlinDu,sayhei,lvchengchang,le0l1,1509477745,dcwang1989,swing4,archer-bao,sszgr,83103005,qq976739120,aredcup,sowhaton,royromny,wx7217242,thhy,rongxr,MonkeyInWind,Fang-Li,hbk100,小考拉"
 
 func init() {
 	userPool = make(map[string]int)
@@ -26,6 +28,7 @@ func init() {
 	fmt.Println(`/_____/  \____/ \__/  |__/|__/  \___/ /_.___/`)
 	fmt.Println(``)
 	fmt.Println(`             → 2019新春抽奖活动 ←              `)
+	fmt.Println(`       → 感谢@聪少 @深圳-track的无私分享 ←      `)
 	fmt.Println(`     https://github.com/devfeel/dotweb      `)
 	fmt.Println(``)
 
@@ -59,7 +62,7 @@ func main() {
 	fmt.Print(" ...3秒")
 	time.Sleep(time.Second)
 	fmt.Println(``)
-	for i := 0; i < 3; i++ {
+	for i := 0; i < 2; i++ {
 		seed := rand.Intn(totalSeed)
 		luckyUser := luckySeed[seed]
 		fmt.Println("用户池:", totalSeed, "随机数:", seed, "获奖用户:", luckyUser)
